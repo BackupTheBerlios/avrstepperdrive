fba658fc3c9bfeb951f3ac44ad04a6f490d4dd5a
hupio <hupio>
2008-10-31 20:58:00 +0000

    Upload Header file for main AVRStepperDriver.c

---
+++ avrstepperdrive/AVRStepperDriver.h
@@ -0,0 +1,17 @@
+/*
+ * AVRStepperDriver.h
+ *
+ *  Created on: Oct 31, 2008
+ *      Author: hut
+ */
+
+#ifndef AVRSTEPPERDRIVER_H_
+#define AVRSTEPPERDRIVER_H_
+
+
+void CommandLine(void);
+void quitCommandLine(void);
+void helpDisplay(void);
+void runMotor(void);
+
+#endif /* AVRSTEPPERDRIVER_H_ */
