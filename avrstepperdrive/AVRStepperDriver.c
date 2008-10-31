eecd8f0d3758cb41366ee642f77fb04ffb54ddc4
hupio <hupio>
2008-10-31 21:01:36 +0000

    Update files and finished skeleton

--- avrstepperdrive/AVRStepperDriver.c
+++ avrstepperdrive/AVRStepperDriver.c
@@ -8,11 +8,17 @@
 
 
 #include <avr/io.h>
+
+
+
 #include "AVRLib\uart.h"
 #include "AVRLib\cmdline.h"
 #include "AVRLib\vt100.h"
 #include "AVRLib\a2d.h"
 #include "AVRLib\rprintf.h"
+#include "AVRLib\timer.h"
+
+#include "AVRStepperDriver.h"
 
 
 
@@ -20,6 +26,7 @@
 u08 Run;
 u08 FullStepArray[4] = { 1,2,4,8};
 
+void CommandLine(void);
 
 int main(void)
 {
@@ -27,12 +34,63 @@ int main(void)
 	uartSetBaudRate(115200);
 	rprintfInit(uartSendByte);
 
-	Run = TRUE;
+	a2dInit();
+	timerInit();
+	vt100Init();
 
-	while(Run)
-	{
 
-	}
+
 
 	return 0;
 }
+
+void CommandLine(void)
+{
+	u08 c;
+	/* Variable to Run or stop program */
+	Run = TRUE;
+
+	/* Clears the screen and sets the cursor under the Title */
+	vt100ClearScreen();
+	vt100SetCursorPos(1,0);
+
+	rprintfProgStrM("\r\n\t\t\tStepper Motor Driver - Serial Console\r\n");
+
+	cmdlineInit();
+	cmdlineSetOutputFunc(uartSendByte);
+
+	cmdlineAddCommand("quit", quitCommandLine);
+	cmdlineAddCommand("help", helpDisplay);
+	cmdlineAddCommand("step", runMotor);
+
+	cmdlineInputFunc('\r');
+
+	while(Run)
+	{
+		while( uartReceiveByte(&c) )
+			cmdlineInputFunc(c);
+
+		cmdlineMainLoop();
+
+	}
+
+	rprintfCRLF();
+	rprintf("Program halted.");
+
+
+}
+
+void quitCommandLine(void)
+{
+
+}
+
+void helpDisplay(void)
+{
+
+}
+void runMotor(void)
+{
+
+}
+
